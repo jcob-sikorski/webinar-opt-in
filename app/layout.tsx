60c85286e184d26c1e22d5068cdda15d511c8e51
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-08-03 22:52:38 +0200

    added page view meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import React, { type ReactNode } from "react";
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
-
+import Script from "next/script";
 import "./globals.css";
-
 import { RegisterModalProvider } from "@/components/register-modal-provider";
 
 const body = Inter({
@@ -21,7 +21,7 @@ const display = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Jak Zbudować Studio Treningu Personalnego Generujące 600 000 zł Rocznie",
+  title: "Jak Zbudowa  Studio Treningu Personalnego Generuj ce 600 000 z  Rocznie",
   description:
     "A 2-hour workshop on the Agreement Engineering webinar framework.",
 };
@@ -29,13 +29,38 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
-   return (
-     <html lang="pl" className={`${body.variable} ${display.variable}`}>
-        <body className="font-sans text-body">
-          <RegisterModalProvider>{children}</RegisterModalProvider>
-        </body>
-     </html>
-   );
+  return (
+    <html lang="pl" className={`${body.variable} ${display.variable}`}>
+      <body className="font-sans text-body">
+        {/* Meta Pixel base code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '965293539900334');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=965293539900334&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        <RegisterModalProvider>{children}</RegisterModalProvider>
+      </body>
+    </html>
+  );
 }
