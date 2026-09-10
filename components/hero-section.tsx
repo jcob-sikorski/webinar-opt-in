fac83e5721a3da0c146893fd230e59a0f433239c
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-09-11 00:33:54 +0200

    reduced number of spots to "9"

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -74,7 +74,7 @@ export function HeroSection() {
                   <span className="relative inline-flex h-3.5 w-3.5 rounded-full bg-red-500" />
                 </span>
                 <span>
-                  Zostało miejsc: <strong className="font-bold text-neutral-900">11</strong>
+                  Zostało miejsc: <strong className="font-bold text-neutral-900">9</strong>
                 </span>
               </div>
             </div>
