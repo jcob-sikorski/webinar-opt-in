b3e080e71d3230580e016bde8d78271f97c3d5cd
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-09-12 07:40:52 +0200

    updated number of spots

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -104,7 +104,7 @@ export function HeroSection() {
                   <span className="relative inline-flex h-3.5 w-3.5 rounded-full bg-red-500" />
                 </span>
                 <span>
-                  Zostało miejsc: <strong className="font-bold text-neutral-900">6</strong>
+                  Zostało miejsc: <strong className="font-bold text-neutral-900">4</strong>
                 </span>
               </div>
             </div>
